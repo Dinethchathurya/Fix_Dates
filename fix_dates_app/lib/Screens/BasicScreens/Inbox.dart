5e70f87b8cad4ee9432e7af2fe283d4ee4160201
Dineth Chathurya <130559325+Dinethchathurya@users.noreply.github.com>
2024-03-01 20:01:19 +0530

    successfully completed event page front end

--- fix_dates_app/lib/Screens/BasicScreens/Inbox.dart
+++ fix_dates_app/lib/Screens/BasicScreens/Inbox.dart
@@ -1,3 +1,4 @@
+import 'package:fix_dates_app/components/Index/events_ListTile_widget.dart';
 import 'package:flutter/material.dart';
 
 class Inbox extends StatelessWidget {
@@ -7,21 +8,79 @@ class Inbox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Text('Events'),
-            TextField(
-              decoration: InputDecoration(
-                icon: Icon(Icons.search),
-                border: OutlineInputBorder().copyWith(
-                  borderRadius: BorderRadius.circular(50.0),
+        child: Container(
+          margin: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Text(
+                    'Events',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
                 ),
-                // hintText: 'Enter a search term',
               ),
-            )
-          ],
+              Container(
+                height: 40.0,
+                child: TextField(
+                  decoration: InputDecoration(
+                      suffixIcon: Icon(
+                        Icons.search,
+                        size: 35.0,
+                      ),
+                      border: OutlineInputBorder().copyWith(
+                        borderRadius: BorderRadius.circular(50.0),
+                      ),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 15.0, horizontal: 50.0)
+                      // hintText: 'Enter a search term',
+                      ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        events_ListTile_widget(),
+                        events_ListTile_widget(),
+                        events_ListTile_widget(),
+                        events_ListTile_widget(),
+                        events_ListTile_widget(),
+                        events_ListTile_widget(),
+                        events_ListTile_widget(),
+                        events_ListTile_widget(),
+                        events_ListTile_widget(),
+                        events_ListTile_widget(),
+                        events_ListTile_widget(),
+                        events_ListTile_widget(),
+                        events_ListTile_widget(),
+                        events_ListTile_widget(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: CircleBorder(),
+        backgroundColor: Colors.black,
+        child: Icon(
+          Icons.add,
+          size: 40.0,
+          color: Colors.white,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
