719958be50bdfa3119474b011a800f6d8c72b501
AshiniGunawardhana <ashinigunawardhana@gmail.com>
2024-04-04 01:25:13 +0530

    I added delete account button to log out screen.
    I added delete account button to log out screen.

--- fix_dates_app/lib/Screens/Settings/LogOut.dart
+++ fix_dates_app/lib/Screens/Settings/LogOut.dart
@@ -11,11 +11,26 @@ class LogOut extends StatelessWidget {
       centerTitle: true, // Center the title
     ),
       body: Center(
-        child: ElevatedButton(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        ElevatedButton(
           onPressed: () {
-            //Perform logout action here
-            //Foe example, you can clear user section or navigate to login page
-            Navigator.pop(context); // Go back to previous screen
+        //Perform delete account action here
+        //For example, you can show a confirmation dialog
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.blue, // background color for delete account button
+        foregroundColor: Colors.white, // Text color for delete account button
+      ),
+      child: Text('Delete Account'),
+    ),
+    SizedBox(height: 20), // Add some space between buttons
+    ElevatedButton(
+    onPressed: () {
+    // Perform logout action here
+    // For example, you can clear user session or navigate to login page
+    Navigator.pop(context); // Go back to the previous screen
           },
           style: ElevatedButton.styleFrom(
             backgroundColor: Colors.blue, // background color
@@ -23,12 +38,14 @@ class LogOut extends StatelessWidget {
           ),
           child: Text('Log Out'),
         ),
+        ],
+        ),
       ),
     );
   }
 }
 
-void main(){
+void main() {
   runApp(MaterialApp(
     home: LogOut(),
   ),
