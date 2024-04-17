7109d0e11483f53663e8c8ba034d8ef662047517
Dineth Chathurya <130559325+Dinethchathurya@users.noreply.github.com>
2024-04-17 19:12:45 +0530

    events can show correct

--- fix_dates_app/lib/Screens/BasicScreens/GetMyEvents.dart
+++ fix_dates_app/lib/Screens/BasicScreens/GetMyEvents.dart
@@ -162,7 +162,6 @@ import 'dart:async'; // Import the dart:async library for StreamSubscription
 
 import 'package:fix_dates_app/Screens/BasicScreens/CreateAnEvent.dart';
 import 'package:fix_dates_app/database/getEvents.dart';
-import 'package:fix_dates_app/polls&createpolls/userpoll.dart';
 import 'package:flutter/material.dart';
 
 class GetMyEvents extends StatefulWidget {
@@ -182,13 +181,6 @@ class _GetMyEventsState extends State<GetMyEvents> {
   @override
   void initState() {
     super.initState();
-    // // Subscribe to the stream and listen for events
-    // eventsSubscription = getEvents.getEvents(widget.groupName).listen((events) {
-    //   // Handle events here, you can set the events to state or perform any other action
-    //   setState(() {
-    //     // Set events to state if needed
-    //   });
-    // });
     var groupName = widget.groupName;
     GetEvents getEvents = GetEvents();
     bbStream = getEvents.getEvents(groupName);
@@ -200,6 +192,25 @@ class _GetMyEventsState extends State<GetMyEvents> {
     // Cancel the subscription when the widget is disposed to avoid memory leaks
     eventsSubscription.cancel();
     super.dispose();
+  }
+
+  bool _hasVoted = false;
+  int _selectedOptionIndex = -1;
+  List<int> _votes = [0, 0]; // Track votes for each option
+  //final List<String> _options = ["Bee honey", "Nikan Honey"];
+
+  void _vote(int optionIndex) {
+    if (!_hasVoted) {
+      setState(() {
+        _hasVoted = true;
+        _selectedOptionIndex = optionIndex;
+        _votes[optionIndex]++;
+      });
+    }
+  }
+
+  int _totalVotes() {
+    return _votes.reduce((value, element) => value + element);
   }
 
   @override
@@ -224,32 +235,6 @@ class _GetMyEventsState extends State<GetMyEvents> {
                 ),
               ),
               Expanded(
-                // child: StreamBuilder<List<Map<String, dynamic>>>(
-                //   stream: getEvents.getEvents(widget.groupName),
-                //   builder: (context, snapshot) {
-                //     if (snapshot.connectionState == ConnectionState.waiting) {
-                //       return Center(child: CircularProgressIndicator());
-                //     } else if (snapshot.hasError) {
-                //       return Center(child: Text('Error: ${snapshot.error}'));
-                //     } else {
-                //       List<Map<String, dynamic>>? events = snapshot.data;
-                //       if (events == null || events.isEmpty) {
-                //         return Center(child: Text('No events found for ${widget.groupName}.'));
-                //       } else {
-                //         return ListView.builder(
-                //           itemCount: events.length,
-                //           itemBuilder: (context, index) {
-                //             return ListTile(
-                //               title: Text(events[index]['eventName']),
-                //               subtitle: Text(events[index]['eventDescription']),
-                //               // Add more event details if needed
-                //             );
-                //           },
-                //         );
-                //       }
-                //     }
-                //   },
-                // ),
                 child: StreamBuilder<List<Map<String, dynamic>>>(
                   stream: bbStream, // Pass the stream to stream parameter
                   builder: (context, snapshot) {
@@ -262,21 +247,113 @@ class _GetMyEventsState extends State<GetMyEvents> {
                       return ListView.builder(
                         itemCount: data!.length,
                         itemBuilder: (context, index) {
-                          var documentId = data[index][
-                              'id']; // Assuming 'id' is the key for document ID in your map
-                          return ListTile(
-                            title: Text('Document ID: $documentId'),
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => UserPoll(
-                                    eventId: documentId,
-                                    groupName: widget.groupName,
-                                  ),
-                                ),
-                              );
-                            },
+                          var documentId = data[index]['id'];
+                          var title = data[index]['title'];
+                          var description = data[index]['description'];
+                          var createdUserId = data[index]['created_user_id'];
+                          List options = data[index]['options'];
+
+                          // Assuming 'id' is the key for document ID in your map
+                          return Container(
+                            padding: const EdgeInsets.all(20),
+                            child: Column(
+                              children: [
+                                ...List.generate(1, (index) {
+                                  return Container(
+                                    margin: const EdgeInsets.only(bottom: 10),
+                                    padding: const EdgeInsets.all(8),
+                                    decoration: BoxDecoration(
+                                        border: Border.all(color: Colors.grey),
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        ListTile(
+                                          contentPadding:
+                                              const EdgeInsets.all(0),
+                                          leading: CircleAvatar(),
+                                          title: Text('$title'),
+                                          trailing: _hasVoted
+                                              ? null
+                                              : IconButton(
+                                                  onPressed: () {},
+                                                  icon:
+                                                      const Icon(Icons.delete),
+                                                ),
+                                        ),
+                                        Text('$description'),
+                                        const SizedBox(
+                                          height: 8,
+                                        ),
+                                        ...List.generate(options.length,
+                                            (optionIndex) {
+                                          return InkWell(
+                                            onTap: () {
+                                              _vote(optionIndex);
+                                            },
+                                            child: Container(
+                                              margin: const EdgeInsets.only(
+                                                  bottom: 5),
+                                              child: Row(
+                                                children: [
+                                                  Expanded(
+                                                    child: Stack(
+                                                      children: [
+                                                        LinearProgressIndicator(
+                                                          minHeight: 30,
+                                                          value: _hasVoted
+                                                              ? _votes[
+                                                                      optionIndex] /
+                                                                  100
+                                                              : 0,
+                                                          backgroundColor:
+                                                              Colors.white,
+                                                        ),
+                                                        Container(
+                                                          alignment:
+                                                              Alignment.center,
+                                                          padding:
+                                                              const EdgeInsets
+                                                                  .symmetric(
+                                                                  horizontal:
+                                                                      10),
+                                                          height: 30,
+                                                          child: Text(options[
+                                                              optionIndex]),
+                                                        )
+                                                      ],
+                                                    ),
+                                                  ),
+                                                  const SizedBox(
+                                                    width: 20,
+                                                  ),
+                                                  Text(
+                                                    _hasVoted
+                                                        ? '${_votes[optionIndex]}%'
+                                                        : 'Tap to Vote',
+                                                    style: _hasVoted
+                                                        ? TextStyle(
+                                                            fontWeight:
+                                                                FontWeight.bold)
+                                                        : null,
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          );
+                                        }),
+                                        _hasVoted
+                                            ? Text(
+                                                "Total Votes: ${_totalVotes()}")
+                                            : const SizedBox()
+                                      ],
+                                    ),
+                                  );
+                                })
+                              ],
+                            ),
                           );
                         },
                       );
--- fix_dates_app/lib/polls&createpolls/userpoll.dart
+++ fix_dates_app/lib/polls&createpolls/userpoll.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:fix_dates_app/polls&createpolls/createpoll.dart';
 
 class UserPoll extends StatefulWidget {
   final String eventId;
   final String groupName;
 
-  const UserPoll({Key? key, required this.eventId, required this.groupName}) : super(key: key);
+  const UserPoll({Key? key, required this.eventId, required this.groupName})
+      : super(key: key);
 
   @override
   State<UserPoll> createState() => _UserPollState();
@@ -27,113 +27,101 @@ class _UserPollState extends State<UserPoll> {
     }
   }
 
-   int _totalVotes() {
+  int _totalVotes() {
     return _votes.reduce((value, element) => value + element);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: CustomScrollView(
-          slivers: [
-            SliverToBoxAdapter(
-              child: Container(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    ...List.generate(1, (index) {
-                      return Container(
-                        margin: const EdgeInsets.only(bottom: 10),
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ListTile(
-                              contentPadding: const EdgeInsets.all(0),
-                              leading: CircleAvatar(),
-                              title: const Text("Dineth"),
-                              trailing: _hasVoted
-                                  ? null
-                                  : IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(Icons.delete),
-                                    ),
-                            ),
-                            const Text("Honey"),
-                            const SizedBox(
-                              height: 8,
-                            ),
-                            ...List.generate(_options.length, (optionIndex) {
-                              return InkWell(
-                                onTap: () {
-                                  _vote(optionIndex);
-                                },
-                                child: Container(
-                                  margin: const EdgeInsets.only(bottom: 5),
-                                  child: Row(
-                                    children: [
-                                      Expanded(
-                                        child: Stack(
-                                          children: [
-                                            LinearProgressIndicator(
-                                              minHeight: 30,
-                                              value: _hasVoted
-                                                  ? _votes[optionIndex] / 100
-                                                  : 0,
-                                              backgroundColor: Colors.white,
-                                            ),
-                                            Container(
-                                              alignment: Alignment.center,
-                                              padding:
-                                                  const EdgeInsets.symmetric(
-                                                      horizontal: 10),
-                                              height: 30,
-                                              child: Text(_options[optionIndex]),
-                                            )
-                                          ],
-                                        ),
-                                      ),
-                                      const SizedBox(
-                                        width: 20,
-                                      ),
-                                      Text(
-                                        _hasVoted
-                                            ? '${_votes[optionIndex]}%'
-                                            : 'Tap to Vote',
-                                        style: _hasVoted
-                                            ? TextStyle(
-                                                fontWeight: FontWeight.bold)
-                                            : null,
-                                      ),
-                                    ],
-                                  ),
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                ...List.generate(1, (index) {
+                  return Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ListTile(
+                          contentPadding: const EdgeInsets.all(0),
+                          leading: CircleAvatar(),
+                          title: const Text("Dineth"),
+                          trailing: _hasVoted
+                              ? null
+                              : IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.delete),
                                 ),
-                              );
-                            }),
-                            _hasVoted
-                                ? Text("Total Votes: ${_totalVotes()}")
-                                : const SizedBox()
-                          ],
                         ),
-                      );
-                    })
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => CreatePoll()));
-        },
-        child: const Icon(Icons.add),
-      ),
+                        const Text("Honey"),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        ...List.generate(_options.length, (optionIndex) {
+                          return InkWell(
+                            onTap: () {
+                              _vote(optionIndex);
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.only(bottom: 5),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Stack(
+                                      children: [
+                                        LinearProgressIndicator(
+                                          minHeight: 30,
+                                          value: _hasVoted
+                                              ? _votes[optionIndex] / 100
+                                              : 0,
+                                          backgroundColor: Colors.white,
+                                        ),
+                                        Container(
+                                          alignment: Alignment.center,
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 10),
+                                          height: 30,
+                                          child: Text(_options[optionIndex]),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 20,
+                                  ),
+                                  Text(
+                                    _hasVoted
+                                        ? '${_votes[optionIndex]}%'
+                                        : 'Tap to Vote',
+                                    style: _hasVoted
+                                        ? TextStyle(fontWeight: FontWeight.bold)
+                                        : null,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        }),
+                        _hasVoted
+                            ? Text("Total Votes: ${_totalVotes()}")
+                            : const SizedBox()
+                      ],
+                    ),
+                  );
+                })
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
